922eee7533408507f8ec7a0241c935795e730ce2
aayushadhikari7 <aayushadhikari2007@gmail.com>
2026-01-21 00:49:58 +0545

    Initial project setup

---
+++ lib/crawler.ex
@@ -0,0 +1,9 @@
+defmodule Crawler do
+  @moduledoc """
+  A concurrent web crawler built to learn Elixir's process model.
+  """
+
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Crawler.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :crawler,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
